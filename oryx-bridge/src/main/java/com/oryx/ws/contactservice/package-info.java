255b805c3ef0ace0a6633e7e0f4981c98b0e2c6b
Sami Mbarki <sami.mbarki@gefco.net>
2018-03-22 14:31:19 +0100

    first commit

---
+++ oryx-bridge/src/main/java/com/oryx/ws/contactservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://com/oryx/ws/contactservice", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.oryx.ws.contactservice;
