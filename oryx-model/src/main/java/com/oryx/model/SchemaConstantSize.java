1633ee606c9aae63e32c2bf0208cf65e82c08981
sami.mbarki@gmail.com <sami2411820>
2018-03-23 11:56:59 +0100

    180323.1059 STABLE

--- oryx-model/src/main/java/com/oryx/model/SchemaConstantSize.java
+++
@@ -1,9 +0,0 @@
-package com.oryx.model;
-
-/**
- * Created by 241180 on 26/01/2017.
- */
-
-public interface SchemaConstantSize extends ComSchemaConstantSize {
-
-}
--- oryx-model/src/main/java/com/oryx/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oryx.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.oryx.model.IUser;
-
-public interface UserRepository extends CrudRepository<IUser, Integer> {
-    IUser findByEmail(String email);
-}
